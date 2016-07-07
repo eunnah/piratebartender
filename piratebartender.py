15be631ab06ebd5cae25a477310cafa1e129caba
eunnah <eunnah@gmail.com>
2016-07-07 05:51:49 +0000

    Initial commit

--- piratebartender.py
+++ piratebartender.py
@@ -1,5 +1,4 @@
 import random
-import sys
 
 questions = {
     "strong": "Do ye like yer drinks strong? ",
@@ -17,55 +16,32 @@ ingredients = {
     "fruity": ["slice of orange", "dash of cassis", "cherry on top"],
 }
 
-answers = {
-}
-
-drink = []
-
 def ask():
     """I ask a series of questions to find out what drink you want"""
-    response = input(questions["strong"])
-    if response == 'y' or response == 'yes' or response == 'YES' or response == "Yes":
-        answers["strong"] = True
-    else:
-        answers["strong"] = False
-    response = input(questions["salty"])
-    if response == 'y' or response == 'yes' or response == 'YES' or response == "Yes":
-        answers["salty"] = True
-    else:
-        answers["salty"] = False
-    response = input(questions["bitter"])
-    if response == 'y' or response == 'yes' or response == 'YES' or response == "Yes":
-        answers["bitter"] = True
-    else:
-        answers["bitter"] = False
-    response = input(questions["sweet"])
-    if response == 'y' or response == 'yes' or response == 'YES' or response == "Yes":
-        answers["sweet"] = True
-    else:
-        answers["sweet"] = False
-    response = input(questions["fruity"])
-    if response == 'y' or response == 'yes' or response == 'YES' or response == "Yes":
-        answers["fruity"] = True
-    else:
-        answers["fruity"] = False
+    answers = {}
+    for type, question in questions.items():
+        response = input(questions[type])
+        if response.lower() == 'y' or response.lower() == 'yes':
+            answers[type] = True
+        else:
+            answers[type] = False
     return answers
         
 def makedrink(answers):
     """I make a drink in response to collected answers"""
-    if answers["strong"] == True:
-        drink.append(random.choice(ingredients["strong"]))
-    if answers["salty"] == True:
-        drink.append(random.choice(ingredients["salty"]))
-    if answers["bitter"] == True:
-        drink.append(random.choice(ingredients["bitter"]))
-    if answers["sweet"] == True:
-        drink.append(random.choice(ingredients["sweet"]))
-    if answers["fruity"] == True:
-        drink.append(random.choice(ingredients["fruity"]))
-    return "Your drink is served: " + str(drink)
+    drink = []
+    
+    for type, liked in answers.items():
+        if not liked:
+            continue
+        drink.append(random.choice(ingredients[type]))
+
+    print("Your drink is served: ")
+    for ingredient in drink:
+        print("A {}".format(ingredient))
+        
+    return drink
     
 if __name__ == '__main__':
     answers = ask()
     drink = makedrink(answers)
-    print(drink)
